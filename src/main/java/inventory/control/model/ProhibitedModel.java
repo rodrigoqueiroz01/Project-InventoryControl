66a5df9bc4f8fd2dffb55f6a1fa2058e40f1d1ba
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2022-07-08 10:45:44 -0300

    Ajustes

--- src/main/java/inventory/control/model/ProhibitedModel.java
+++ src/main/java/inventory/control/model/ProhibitedModel.java
@@ -5,9 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
