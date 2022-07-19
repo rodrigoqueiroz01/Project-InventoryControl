1dee1b0b9fe76facde0f769f8b27e9c396622125
rodrigoqueiroz01 <rodrigoq1212@gmail.com>
2022-07-18 21:27:13 -0300

    Config

--- src/main/java/inventory/control/config/OpenApiConfig.java
+++ src/main/java/inventory/control/config/OpenApiConfig.java
@@ -1,9 +1,7 @@
 package inventory.control.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
